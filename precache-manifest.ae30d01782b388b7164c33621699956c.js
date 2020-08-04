a9aadc038ca8ad90962b4788c14bf0f55bc91c2c
Pakupoko <tjlee1119@gmail.com>
2020-08-04 21:08:07 +0900

    Updates

---
+++ precache-manifest.ae30d01782b388b7164c33621699956c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22643512465440a2bc538f051d3fee3e",
+    "url": "/movie_app_tj/index.html"
+  },
+  {
+    "revision": "3827a8309dc450a1b459",
+    "url": "/movie_app_tj/static/css/main.1035ac38.chunk.css"
+  },
+  {
+    "revision": "49ea343b12925877b7be",
+    "url": "/movie_app_tj/static/js/2.8394b7bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_tj/static/js/2.8394b7bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3827a8309dc450a1b459",
+    "url": "/movie_app_tj/static/js/main.9a22da7b.chunk.js"
+  },
+  {
+    "revision": "c6581ddfeb3588881ef4",
+    "url": "/movie_app_tj/static/js/runtime-main.40ca1779.js"
+  }
+]);
